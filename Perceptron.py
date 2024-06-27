a4582d00ef4d490f88a8d8c286cdd3c598480a11
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-06-27 18:01:08 +0200

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,1 +1,7 @@
-
+import torch
+import torch.nn as nn
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+from datasets import load_dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
