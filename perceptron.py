d12d1a8ff00906ba5b344246cfb95ea6bae2f656
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-05 15:46:38 +0200

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -14,14 +14,24 @@ import random
 import os
 from pywttr import Wttr
 
+# Function for Perceptron's voice
 def say(text):
     engine.say(text)
     engine.runAndWait()
 
+# Manual function for the weather
 def weather():
     engine.say("The average temperature in Vera is " + forecast.weather[0].avgtemp_c + " degrees celsius")
     engine.runAndWait()
 
+# Manual function for playing music
 def sound(path):
     mixer.music.load(path)
     mixer.music.play()
+
+# Function to generate responses
+def generate_response(prompt, model, tokenizer):
+    inputs = tokenizer.encode(prompt, return_tensors='pt').to(device)
+    outputs = model.generate(inputs, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
