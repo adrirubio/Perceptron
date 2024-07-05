33e42c65330480b4d9c628514985799c1053bea7
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-05 15:27:00 +0200

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,15 @@
-
+# Add necessary imports
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import speech_recognition as sr
+import pyaudio
+import pywhatkit
+import datetime
+import pyttsx3
+from pygame import mixer
+import wikipedia
+import time
+import vlc
+import random
+import os
+from pywttr import Wttr
