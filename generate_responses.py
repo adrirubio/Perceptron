a68d1a8909177030cf4938d166f1246e32bb918d
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-05 11:31:46 +0200

    Update generate_responses.py

--- generate_responses.py
+++ generate_responses.py
@@ -1,1 +1,11 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+# Load GPT-2 tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+# Load the trained model weights
+model_save_path = "gpt2_dailydialog.pt"
+model.load_state_dict(torch.load(model_save_path))
+model.eval()
