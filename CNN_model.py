6631ad3a84d33e27ffc6e8d5d7a8bdc0e1ed95a5
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-06 22:40:37 +0200

    Update CNN_model.py

--- CNN_model.py
+++ CNN_model.py
@@ -5,3 +5,11 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+# Define transformations for data augmentation and normalization
+transformer_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomAffine(0, translate=(0.1, 0.1)),
+    transforms.ToTensor(),
+])
