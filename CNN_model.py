b94b22486ad4682b6c731c1ca2e266700696b802
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-10 11:48:33 +0200

    Update CNN_model.py

--- CNN_model.py
+++ CNN_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
