0afaa3d40df5ad4e59c61e46259f55bbf4c52f99
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-10 21:19:50 +0200

    Update sentiment_analisis_model.py

--- sentiment_analisis_model.py
+++ sentiment_analisis_model.py
@@ -7,3 +7,10 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import requests
 import io
+
+# Load the SST-2 dataset
+dataset = load_dataset("glue", "sst2")
+
+# Split into train and test sets
+train_dataset = dataset['train']
+test_dataset = dataset['test']
