3360f1290f418d507814f2569be32b3aa277f544
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-14 12:10:38 +0200

    Update sentiment_analisis_model.py

--- sentiment_analisis_model.py
+++ sentiment_analisis_model.py
@@ -159,3 +159,24 @@ plt.show()
 model_save_path = "sentiment_analisis_model.pth"
 torch.save(model.state_dict(), model_save_path)
 print(f"Model saved to {model_save_path}")
+
+# Accuracy
+model.eval()
+n_correct = 0.
+n_total = 0.
+for inputs, targets in train_loader_fixed:
+  # Move to GPU
+  inputs, targets = inputs.to(device), targets.to(device)
+
+  # Forward pass
+  outputs = model(inputs)
+
+  # Get prediction
+  # torch.max returns both max and argmax
+  _, predictions = torch.max(outputs, 1)
+
+  # update counts
+  n_correct += (predictions == targets).sum().item()
+  n_total += targets.shape[0]
+
+train_acc = n_correct / n_total
