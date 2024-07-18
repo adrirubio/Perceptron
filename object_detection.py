4b61b8a1764e575d573fc23e659c9f50f21f9fe5
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-07-18 22:47:53 +0200

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,1 +1,8 @@
-
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from datasets import load_dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
