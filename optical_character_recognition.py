8bdb6b22e6fa3f1f2c50901b0483b63c7cf2222c
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-08-02 23:32:47 +0200

    Create optical_character_recognition.py

---
+++ optical_character_recognition.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
