bd32b991728ad7ac42da2dbe542161dbd02d00ed
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-08-04 16:42:45 +0200

    Update optical_character_recognition.py

--- optical_character_recognition.py
+++ optical_character_recognition.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
