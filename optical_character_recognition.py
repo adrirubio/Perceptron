69a70efe32c5eff8623244a757bce787914e01f2
Adrian Rubio-Punal <76663380+adrirubio@users.noreply.github.com>
2024-08-05 18:26:15 +0200

    Update optical_character_recognition.py

--- optical_character_recognition.py
+++ optical_character_recognition.py
@@ -74,3 +74,50 @@ for batch in test_loader:
     print(inputs.shape)
     print(targets)
     break
+
+# Define the model with more but smaller convolutions
+class CNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32)
+        )
+        self.conv2 = nn.Sequential(
+            nn.conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64)
+        )
+        self.conv3 = nn.Sequential(
+            nn.conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128)
+        )
+        self.conv3 = nn.Sequential(
+            nn.conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256)
+        )
+        self.conv4 = nn.Sequential(
+            nn.conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(512)
+        )
+
+        self.pool = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(512 * 4 * 4, 1024)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.pool(self.conv1(x))
+        x = self.pool(self.conv2(x))
+        x = self.pool(self.conv3(x))
+        x = self.pool(self.conv4(x))
+        x = self.pool(self.conv5(x))
+        x = x.view(x.size(0), -1)
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
